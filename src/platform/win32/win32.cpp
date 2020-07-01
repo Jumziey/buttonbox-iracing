2c9d47ce16cac5fe622d12c74a1aa75ca4dac2e3
jumziey <jumziey@gmail.com>
2020-07-01 15:59:48 +0200

    Platform/Win32 now register mouse position even if do not have focus (since we should work without it).

--- src/platform/win32/win32.cpp
+++ src/platform/win32/win32.cpp
@@ -187,10 +187,9 @@ static void ImGui_ImplWin32_UpdateMousePos() {
   // Set mouse position
   io.MousePos = ImVec2(-FLT_MAX, -FLT_MAX);
   POINT pos;
-  if (HWND active_window = ::GetForegroundWindow())
-    if (active_window == g_hWnd || ::IsChild(active_window, g_hWnd))
-      if (::GetCursorPos(&pos) && ::ScreenToClient(g_hWnd, &pos))
-        io.MousePos = ImVec2((float)pos.x, (float)pos.y);
+  // We take position even if  we don't have focus
+  if (::GetCursorPos(&pos) && ::ScreenToClient(g_hWnd, &pos))
+    io.MousePos = ImVec2((float)pos.x, (float)pos.y);
 }
 
 // Gamepad navigation mapping
